07a0a4ae677ad1f32cf9ee7276dd8f71be20896a
luisa deschamps <andradeps434@gmail.com>
2025-06-11 23:47:00 -0300

    problemas na insercao de data nascimento aluno

--- src/main/java/com/lulocas/GerenciadorEscolar/model/Aluno.java
+++ src/main/java/com/lulocas/GerenciadorEscolar/model/Aluno.java
@@ -1,12 +1,12 @@
 package com.lulocas.GerenciadorEscolar.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -33,14 +33,21 @@ public class Aluno {
     @Column(name="ano_escola", nullable=false)
     private int ano;
 
-    @Column(name="data_nascimento", nullable=false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name="data_nascimento")
     private LocalDate dataNascimento;
 
-    @Column(name="email", length=100, nullable=false)
+    @Column(name="email", length=100)
     private String email;
 
+    @Column(name="senha", length=20)
+    private String senha;
+
+    @Column(name="telefone", length=20)
+    private String telefone;
+
     @ManyToOne
-    @JoinColumn(name="turma_id", nullable=false)
+    @JoinColumn(name="turma_id")
     private Turma turma;
 
 
